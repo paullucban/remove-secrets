cc54248c92da09975fe58997f880a26d5a3081ab
PaulLucban <paul.lucban@appvantage.asia>
2018-10-03 17:33:21 +0800

    remove connection
    remove connection

--- start_app_server.sh
+++ start_app_server.sh
@@ -2,9 +2,5 @@
 # README
 
 # READ SECRET!
-export MONGODB_CREDENTIALS=mongodb://{{USER}}:{{PASSWORD}}@ds666666.mlab.com:39999/some-database
-
 # finally, run the app in dev mode...
 npm run dev
-
-#CommitYourFirstSecret
